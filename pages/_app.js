e244e56408a1d4c937e2629127a24b3672189ef7
Kyle Kelley <kylekelley711@gmail.com>
2020-09-04 10:11:32 -0700

    Initial Git Push

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,37 @@
-import '../styles/globals.css'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Layout } from '../src/layout';
+import Head from 'next/head';
+// import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function MyApp (props) {
+  const { Component, pageProps } = props 
+
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side')
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles)
+    }
+  }, []);
+
+  return(
+    <>
+        <Head>
+          {/* PWA primary color */}
+          {/* <meta name="theme-color" content={theme.palette.primary.main} /> */}
+            <title>Brynn Kerin / Wellness Advocate</title>
+            <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width' />
+        </Head>
+      <Layout />
+        <Component {...pageProps} />
+      
+    </>
+  )
 }
 
-export default MyApp
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired
+}
+
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,29 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import React from 'react'
+import Paper from '@material-ui/core/Paper';
+import { styled } from '@material-ui/core/styles';
+//import styles from '../styles/Home.module.css'
+
+const MyPaper = styled(Paper)({
+  background: '#fffdd0',
+  width: '85vw',
+  height: '85vh',
+  margin: '50px auto',
+  borderRadius: '10px',
+  textAlign: 'center'
+});
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div>
+      <MyPaper>
+        <h1>Hello I'm Brynn Suckas!</h1>
+      </MyPaper>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      <style jsx global>{`
+        body {
+          background: #038a2d;
+        }
+      `}</style>
     </div>
   )
 }
